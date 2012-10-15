62a1c318ca70f19c987b52903aaadd33b366bb8e
Roxana Geambasu <roxana@roxana-desktop.cs.columbia.edu>
2012-10-14 20:59:47 -0400

    Lab 3.

---
+++ l3/yfs_client.h
@@ -0,0 +1,62 @@
+#ifndef yfs_client_h
+#define yfs_client_h
+
+#include <string>
+//#include "yfs_protocol.h"
+#include "extent_client.h"
+#include <vector>
+
+#line 13 "../yfs_client.h"
+
+#line 21 "../yfs_client.h"
+class yfs_client {
+  extent_client *ec;
+#line 24 "../yfs_client.h"
+ public:
+
+  typedef unsigned long long inum;
+  enum xxstatus { OK, RPCERR, NOENT, IOERR, FBIG, EXIST };
+  typedef int status;
+
+  struct fileinfo {
+    unsigned long long size;
+    unsigned long atime;
+    unsigned long mtime;
+    unsigned long ctime;
+  };
+  struct dirinfo {
+    unsigned long atime;
+    unsigned long mtime;
+    unsigned long ctime;
+  };
+  struct dirent {
+    std::string name;
+    unsigned long long inum;
+  };
+
+ private:
+  static std::string filename(inum);
+  static inum n2i(std::string);
+#line 50 "../yfs_client.h"
+  inum ilookup(inum di, std::string name);
+#line 55 "../yfs_client.h"
+ public:
+#line 59 "../yfs_client.h"
+
+  yfs_client(std::string, std::string);
+
+  bool isfile(inum);
+  bool isdir(inum);
+
+  int getfile(inum, fileinfo &);
+  int getdir(inum, dirinfo &);
+#line 68 "../yfs_client.h"
+  int readdir(inum, unsigned long long ind, 
+	      dirent &);
+  int lookup(inum, std::string name, inum &);
+  int create(inum, std::string name, int dir, 
+	     inum &);
+#line 83 "../yfs_client.h"
+};
+
+#endif 
